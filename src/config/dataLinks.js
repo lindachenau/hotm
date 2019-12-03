58cb459481a8ce252203825a4162a9a5d2224f42
Linda Chen <lindachenau@gmail.com>
2019-12-03 14:47:03 +1100

    added link for updating metadata

--- src/config/dataLinks.js
+++ src/config/dataLinks.js
@@ -1,12 +1,13 @@
-export const services_url = "https://procomhost.com/hotm_booking/api/v0.2/services"
-// export const artists_url = "https://procomhost.com/hotm_booking/api/v0.2/artists"
-// export const bookings_url = "https://procomhost.com/hotm_booking/api/v0.2/bookings"
-export const artists_url = 'http://localhost:5000/artists'
-export const bookings_url = 'http://localhost:5000/bookings'
+export const services_url = "https://procomhost.com/hotm_booking/api/v0.3/services"
+export const artists_url = "https://procomhost.com/hotm_booking/api/v0.3/artists"
+export const bookings_url = "https://procomhost.com/hotm_booking/api/v0.3/bookings"
+// export const artists_url = 'http://localhost:5000/artists'
+// export const bookings_url = 'http://localhost:5000/bookings'
 export const clients_url = 'http://localhost:5000/clients'
-export const available_artists_url = 'https://procomhost.com/hotm_booking/api/v0.2/get_available_artists.php'
-export const register_nonce_url = "/api/get_nonce/?controller=user&method=register"
-export const register_url = "/api/user/register"
-export const auth_url = "/api/auth/generate_auth_cookie"
-export const reset_pw_url = "/api/user/retrieve_password/?user_login="
+export const available_artists_url = 'https://procomhost.com/hotm_booking/api/v0.3/get_available_artists.php'
+export const register_nonce_url = "https://procomhost.com/api/get_nonce/?controller=user&method=register"
+export const register_url = "https://procomhost.com/api/user/register"
+export const update_user_meta_url = "https://procomhost.com/api/user/update_user_meta_vars"
+export const auth_url = "https://procomhost.com/api/auth/generate_auth_cookie"
+export const reset_pw_url = "https://procomhost.com/api/user/retrieve_password/?user_login="
 export const home_url = "https://procomhost.com"
