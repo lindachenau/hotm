09354e5dc34deeb67e3e7833a88730d188ebd17e
Linda Chen <lindachenau@gmail.com>
2020-01-21 16:57:45 +1100

    Updates

---
+++ precache-manifest.197027e5d277445cb69186b3e1658003.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85f64296bc3e8673d89237306363e894",
+    "url": "/hotm/index.html"
+  },
+  {
+    "revision": "4b9ffaefa64205930bf2",
+    "url": "/hotm/static/css/11.eeff2320.chunk.css"
+  },
+  {
+    "revision": "9cf0dbe2e36dec8bb5b0",
+    "url": "/hotm/static/css/7.dc07e40f.chunk.css"
+  },
+  {
+    "revision": "d88d08e0af6af6caf1b3",
+    "url": "/hotm/static/css/main.1a8d6906.chunk.css"
+  },
+  {
+    "revision": "8facd66e48df7aef1eff",
+    "url": "/hotm/static/js/0.bce66e09.chunk.js"
+  },
+  {
+    "revision": "61ed967934ad7be79054",
+    "url": "/hotm/static/js/1.0e8040f5.chunk.js"
+  },
+  {
+    "revision": "ac117d8a4916dbef9581",
+    "url": "/hotm/static/js/10.8cef6dfc.chunk.js"
+  },
+  {
+    "revision": "4b9ffaefa64205930bf2",
+    "url": "/hotm/static/js/11.3ca5ec1d.chunk.js"
+  },
+  {
+    "revision": "a3e32ec7c20d3db85af4",
+    "url": "/hotm/static/js/2.5a16e246.chunk.js"
+  },
+  {
+    "revision": "df898f06b2830be0d351",
+    "url": "/hotm/static/js/3.c525eb24.chunk.js"
+  },
+  {
+    "revision": "6dad31567ae65199e33d",
+    "url": "/hotm/static/js/6.a131f968.chunk.js"
+  },
+  {
+    "revision": "9cf0dbe2e36dec8bb5b0",
+    "url": "/hotm/static/js/7.e900fa7d.chunk.js"
+  },
+  {
+    "revision": "8f20922b361ddb201467",
+    "url": "/hotm/static/js/8.5d4ba81f.chunk.js"
+  },
+  {
+    "revision": "5373135ce425f0de379b",
+    "url": "/hotm/static/js/9.d67b9095.chunk.js"
+  },
+  {
+    "revision": "d88d08e0af6af6caf1b3",
+    "url": "/hotm/static/js/main.d2d4069b.chunk.js"
+  },
+  {
+    "revision": "cb3ae41ff8f9456800a2",
+    "url": "/hotm/static/js/runtime-main.dc3c7c29.js"
+  },
+  {
+    "revision": "a36b58329db5080156a2e35a989e907b",
+    "url": "/hotm/static/media/logo.a36b5832.png"
+  },
+  {
+    "revision": "4d16281676efe3318b374604f9159e22",
+    "url": "/hotm/static/media/logo192.4d162816.png"
+  }
+]);
