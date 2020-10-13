9f44e89cb951969098dc037e6d7ff40e9f85935e
Linda Chen <lindachenau@gmail.com>
2020-10-14 10:24:42 +1100

    Choose Therapist complete flow

---
+++ src/config/TimeSelectionContainer.js
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux'
+import { submitBooking } from '../actions/bookingCreator'
+import TimeSelection from '../components/TimeSelection'
+
+const mapStateToProps = state => {
+  return {
+    bookingDateAddr: state.bookingDateAddr,
+    itemQty: state.itemQty
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+      submitBooking: (artistStart, date, bookingEnd, addr) => dispatch(submitBooking(artistStart, date, bookingEnd, addr))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TimeSelection)
